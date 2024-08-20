7e1d2b82ee78cf59e8f9de7baf4cb496242ad867
Ahmed Hassan <ahmedabdelrhman433@gmail.com>
2024-08-21 00:12:23 +0300

    2nd task

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -10,15 +10,29 @@ class FIFOCache(BaseCaching):
     caching system
     """
     def __init__(self):
+        """
+        initialize method
+        """
         super().__init__()
 
     def put(self, key, item):
+        """
+        Put values method
+        """
         if key is None or item is None:
             pass
-        else:
+
+        if BaseCaching.MAX_ITEMS > len(self.cache_data):
             self.cache_data[key] = item
+        else:
+            first_item = list(self.cache_data.keys())[0]
+            del self.cache_data[first_item]
+            print(f"DISCARD {first_item}")
 
     def get(self, key):
+        """
+        Get values method
+        """
         if key is None:
             return None
 
