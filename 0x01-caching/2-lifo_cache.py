3bade4b113c7c468d010f3f17490dc493b981fba
Ahmed Hassan <ahmedabdelrhman433@gmail.com>
2024-08-21 00:43:35 +0300

    3rd task

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""
+Basic dictionary
+"""
+from base_caching import BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """
+    caching system
+    """
+    def __init__(self):
+        """
+        initialize method
+        """
+        super().__init__()
+
+    def put(self, key, item):
+        """ Add an item in the cache """
+        if key and item:
+            self.cache_data[key] = item
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            discarded_key = sorted(self.cache_data)[-2]
+            self.cache_data.pop(discarded_key)
+            print('DISCARD: {}'.format(discarded_key))
+
+    def get(self, key):
+        """
+        Get values method
+        """
+        return self.cache_data.get(key, None)
