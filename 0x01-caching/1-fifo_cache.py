ddd800700ca4645241dd60fbcb8054e68d895201
Ahmed Hassan <ahmedabdelrhman433@gmail.com>
2024-08-21 00:29:54 +0300

    2nd task

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -22,7 +22,7 @@ class FIFOCache(BaseCaching):
         if not key and not item:
             return
         self.cache_data[key] = item
-        if BaseCaching.MAX_ITEMS <= len(self.cache_data):
+        if BaseCaching.MAX_ITEMS < len(self.cache_data):
             first_item = list(self.cache_data.keys())[0]
             del self.cache_data[first_item]
             print(f"DISCARD {first_item}")
